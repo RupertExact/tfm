c0707f14693f22a38b4f7e5778d4d0856a3548b9
Rupert Broad <rupert.broad@exact.com>
2017-06-26 00:27:39 +0200

    update ec2 variables

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -75,6 +75,6 @@ variable "dataToken" {
 
 
 // Variables for providers used in this module
-#variable "aws_access_key" {}
-#variable "aws_secret_key" {}
+// variable "aws_access_key" {}
+// variable "aws_secret_key" {}
 variable "aws_region" {}
