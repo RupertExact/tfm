23a5710f3cfb9abe7e273f935a3329c785519793
Rupert Broad <rupert.broad@exact.com>
2017-06-26 23:26:29 +0200

    Updated tags and name variable

--- modules/sg_ssh/main.tf
+++ modules/sg_ssh/main.tf
@@ -1,8 +1,9 @@
 // Security Group Resource for Module
 resource "aws_security_group" "main_security_group" {
-  name        = "${var.security_group_name}"
+  name        = "${var.name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+  tags                 = "${merge(var.tags, map("Name", format("%s", var.name)))}"
 
   // allows traffic for TCP 22 (SSH)
   ingress {
@@ -10,5 +11,12 @@ resource "aws_security_group" "main_security_group" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${var.source_cidr_block}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
